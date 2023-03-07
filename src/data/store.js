21398ac5caa3f5f8cebad1b434ea2173d225f280
Ricardo <121136503+Ricardo0429@users.noreply.github.com>
2023-03-06 19:22:06 -0500

    Update store.js

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import rootReducer from './reducers';
